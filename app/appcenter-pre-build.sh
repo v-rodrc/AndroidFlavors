3042e3eb5f935edcecbda5b87c42babb7ee2a73f
Christopher Rodriguez <christopher.rodriguez@softpository.com>
2020-05-14 19:14:51 -0500

    added search function to pre build script

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -3,5 +3,7 @@ echo "listingdirectory"
 ls
 echo "listingenvironmentvariables"
 printenv
+echo "searchforgradlew"
+find / iname gradlew -type f
 echo "beginbuild"
 ./gradlew bundleChocolateApple --debug
